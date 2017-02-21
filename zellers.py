9bfcf7874723994b7a4db11079fcedffbb2efdf2
Stephen Cerruti <scerruti@e3civichigh.com>
2017-02-21 11:06:48 -0800

    Added recipe statement numbers

--- zellers.py
+++ zellers.py
@@ -5,30 +5,31 @@
 
 # Get the user's birthday.
 
-# Ask the user for the month of his birth as a number, store it in a temporary variable
+# 1) Ask the user for the month of his birth as a number, store it in a temporary variable
 
-# Set A to the month using the following conversion, with March having the value 1, April the
+# 2) Set A to the month using the following conversion, with March having the value 1, April the
 # value 2, . . ., December the value 10, and January and February being
 # counted as months 11 and 12 of the preceding year (in which
 # case,subtract 1 from C)
 
-# Ask the user for the day of his birth, store it in variable B
+# 3) Ask the user for the day of his birth, store it in variable B
 
-# Ask the user for the year of his birth, store it in variable C
+# 4) Ask the user for the year of his birth, store it in variable C
 # remember to subtract 1 as necessary
 
-# If C is greater than 99, then set D (the century) equal to C / 100 (hint: use integer division)
+# 5) If C is greater than 99, then set D (the century) equal to C / 100 (hint: use integer division)
 # Then set C equal to the year of the century (0-99) (hint: use modulo)
 
-# Implement Zeller's algorithm
+# 6) Implement Zeller's algorithm
 # W = (13*A - 1) / 5
 # X = C / 4
 # Y = D / 4
 # Z = W + X + Y + B + C - 2*D
 # R = the remainder when Z is divided by 7
 
-# The value of R is the day of the week, where 0 represents Sunday, 1 is Monday, . . ., 6 is Saturday. If
+# 7) The value of R is the day of the week, where 0 represents Sunday, 1 is Monday, . . ., 6 is Saturday. If
 # the computed value of R is a negative number, add 7 to get a non negative number between 0 and 6 (you
-# don’t need to do this in the code). Print out R. You can check to be sure your code is working by looking at
+# don't need to do this in the code). Print out R. You can check to be sure your code is working by looking at
 # http://www.timeanddate.com/calendar/.
-# Run some test cases- try today’s date, your birth date, and whatever else interests you!
+
+# Run some test cases- try today's date, your birth date, and whatever else interests you!
